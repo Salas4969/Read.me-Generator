c9bf015c590c495f0f95d30578981c4b75727e35
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-06-26 11:33:22 -0400

    prettier

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,57 +3,53 @@ const inq = require("inquirer");
 const fs = require("fs");
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license==="none"||null){
-  return ""
-  }else if(license==="MIT"){
-    return "https://img.shields.io/badge/license-MIT-green"
+  if (license === "none" || null) {
+    return "";
+  } else if (license === "MIT") {
+    return "https://img.shields.io/badge/license-MIT-green";
+  } else if (license === "Eclipse-Public") {
+    return "https://img.shields.io/badge/license-Eclipse--Public-lightgrey";
+  } else if (license === "Apache") {
+    return "https://img.shields.io/badge/license-Apache-blue";
+  } else if (license === "Mozilla-Public") {
+    return "https://img.shields.io/badge/license-Mozilla--Public-yellow";
   }
-else if(license==="Eclipse-Public"){
-  return "https://img.shields.io/badge/license-Eclipse--Public-lightgrey"
-}else if(license==="Apache"){
-  return "https://img.shields.io/badge/license-Apache-blue"
-}else if(license==="Mozilla-Public"){
-  return "https://img.shields.io/badge/license-Mozilla--Public-yellow"
-}
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license==="none"||null){
-  return ""
-}else if(license==="MIT"){
-   return "https://choosealicense.com/licenses/mit/"
-}
-else if(license==="Eclipse-Public"){
- return "https://choosealicense.com/licenses/epl-2.0/"
-}else if(license==="Apache"){
- return "https://choosealicense.com/licenses/apache-2.0/"
-}else if(license==="Mozilla-Public"){
- return "https://choosealicense.com/licenses/mpl-2.0/"
-}
+  if (license === "none" || null) {
+    return "";
+  } else if (license === "MIT") {
+    return "https://choosealicense.com/licenses/mit/";
+  } else if (license === "Eclipse-Public") {
+    return "https://choosealicense.com/licenses/epl-2.0/";
+  } else if (license === "Apache") {
+    return "https://choosealicense.com/licenses/apache-2.0/";
+  } else if (license === "Mozilla-Public") {
+    return "https://choosealicense.com/licenses/mpl-2.0/";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license==="none"||null){
-return ""
-}else if(license==="MIT"){
-  return "A short and simple permissive  with conditions only requiring preservation of copyright and == notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
-}
-else if(license==="Eclipse-Public"){
-return "This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones"
-}else if(license==="Apache"){
-return "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
-}else if(license==="Mozilla-Public"){
-return "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work."
-}
+  if (license === "none" || null) {
+    return "";
+  } else if (license === "MIT") {
+    return "A short and simple permissive  with conditions only requiring preservation of copyright and == notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+  } else if (license === "Eclipse-Public") {
+    return "This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones";
+  } else if (license === "Apache") {
+    return "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+  } else if (license === "Mozilla-Public") {
+    return "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  
   return `
 # ${data.title}
 
