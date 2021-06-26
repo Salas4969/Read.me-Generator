a06a1d3d097963455cc7a2ab1dc923a892aca733
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2021-06-26 11:15:11 -0400

    app should work fully

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,30 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const inq = require("inquirer");
+const fs = require("fs");
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license=""||null)
+  if(license="none"||null){
   return ""
-  else{}
+  }else if(license="MIT"){
+    return "https://img.shields.io/badge/license-MIT-green"
+  }
+else if(license="Eclipse-Public"){
+  return "https://img.shields.io/badge/license-Eclipse--Public-lightgrey"
+}else if(license="Apache"){
+  return "https://img.shields.io/badge/license-Apache-blue"
+}else if(license="Mozilla-Public"){
+  return "https://img.shields.io/badge/license-Mozilla--Public-yellow"
+}
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license=""||null)
+  if(license="none"||null){
   return ""
-  else{}
+}else if(license="MIT"){
+   return "https://choosealicense.com/licenses/mit/"
+}
+else if(license="Eclipse-Public"){
+ return "https://choosealicense.com/licenses/epl-2.0/"
+}else if(license="Apache"){
+ return "https://choosealicense.com/licenses/apache-2.0/"
+}else if(license="Mozilla-Public"){
+ return "https://choosealicense.com/licenses/mpl-2.0/"
+}
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license=""||null)
+  if(license="none"||null){
 return ""
-else{}
+}else if(license="MIT"){
+  fs.appendFile(answers.Title+".md", "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.")
+}
+else if(license="Eclipse-Public"){
+fs.appendFile(answers.Title+".md", "This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones")
+}else if(license="Apache"){
+fs.appendFile(answers.Title+".md", "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.")
+}else if(license="Mozilla-Public"){
+fs.appendFile(answers.Title+".md", "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.")
+}
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+  return `
+# ${data.title}
+
+# Description
+${data.description}
+
+#Table of Content
+* [Description](#Description)
+* [Usage](#usage)
+* [Contribution](#Contribution)
+* [Test](#Test)
+* [License](#license)";
+
+# Usage Information
+${data.usage}
+
+# Contribution Information
+${data.contribution}
+
+# Test Information
+${data.test}
+
+# License
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
 `;
 }
 
--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 // TODO: Include packages needed for this application
 const inq = require("inquirer");
 const fs = require("fs");
-const markdown = require('./generateMarkdown.js')
+// const markdown = require('./generateMarkdown.js');
+const generateMarkdown = require("./generateMarkdown.js");
 // TODO: Create an array of questions for user input
 //installation instructions, usage information, contribution guidelines, and test instructions
 const questions = [
   {
     type: "input",
     message: "Enter a title",
-    name: "Title",
+    name: "title",
   },
   {
     type: "input",
     message: "Enter a descripton",
-    name: "Description",
+    name: "description",
   },
   {
     type: "list",
     message: "Choose a License",
-    name: "License",
-    choices: ["MIT", "Mozilla-Public", "Eclipse Public", "Apache"],
+    name: "license",
+    choices: ["MIT", "Mozilla-Public", "Eclipse Public", "Apache","none"],
   },
   {
     type: "input",
     message: "Enter usage information",
-    name: "Usage",
+    name: "usage",
   },
   {
     type: "input",
     message: " Enter contribution guidelines",
-    name: "Contribution",
+    name: "contribution",
   },
   {
     type: "input",
     message: " Enter test instructions",
-    name: "Test",
+    name: "test",
   },
 ];
 // TODO: Create a function to write README file
@@ -46,41 +47,14 @@ function myErrorFunc(error) {
   }
 }
 function writeToFile(answers) {
-    let desc;
     
-  console.log(answers);
-
-  desc = "#Title\n" + answers.Title;
-
-  fs.writeFile(answers.Title + ".md", desc, myErrorFunc);
-
-  desc = "\n#Description\n" + answers.Description;
-
-  fs.appendFile(answers.Title + ".md", desc, myErrorFunc);
-
-  const contentTable =
-    " #Table of Content\n * [Installation](#installation)\n * [Usage](#usage)\n * [Credits](#credits)\n * [License](#license)";
-
-  fs.appendFile(answers.Title + ".md", contentTable, myErrorFunc);
-
-  const usageInfo = "#Usage Information\n" + answers.Usage;
-
-  fs.appendFile(answers.Title + ".md", usageInfo, myErrorFunc);
-
-  const contributionGuide = "\n#Contribution Guidelines\n" + answers.Contribution
-
-fs.appendFile(answers.Title + ".md", contributionGuide, myErrorFunc);
-
-  const testInfo = "\n#Test Information\n" + answers.Test;
-
-  fs.appendFile(answers.Title + ".md", testInfo, myErrorFunc);
+const readMeCont = generateMarkdown(answers)
+fs.writeFile(answers.Title + ".md", readMeCont, myErrorFunc);
 
 }
 
 // TODO: Create a function to initialize app
 function init() {
-//   inq.prompt(questions).then((answers) => writeToFile(markdown(answers)))
-//   inq.prompt(questions).then((answers) => markdown(answers))
   inq.prompt(questions).then((answers) => writeToFile(answers))
 }
 // Function call to initialize app
